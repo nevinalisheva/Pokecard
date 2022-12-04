47868ae88d78291cb2239f95fc6c3269bc71891b
Nevin Alisheva <nevin.alisheva@gmail.com>
2022-12-04 12:39:35 +0100

    pokecard hide only one element

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,16 +1,11 @@
 import "./Pokecard.css";
-import { useState } from "react";
 
 import React from "react";
 
 export default function Pokecard(props) {
-  const [isHidden, setIsHidden] = useState(false);
-  const toggleShow = () => {
-    setIsHidden((prevState) => !prevState);
-  };
   return (
     <div className="pokecard-container">
-      <div className={`pokemon-details ${isHidden && "hide"}`}>
+      <div className={`pokemon-details ${props.isHidden && "hide"}`}>
         <h4> {props.name} </h4>
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`}
@@ -21,7 +16,10 @@ export default function Pokecard(props) {
           <li>Exp: {props.base_experience}</li>
         </ul>
       </div>
-      <button onClick={toggleShow}> {isHidden ? "show" : "hide"} </button>
+      <button onClick={props.buttonClick}>
+        {" "}
+        {props.isHidden ? "show" : "hide"}{" "}
+      </button>
     </div>
   );
 }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Pokecard from "./Pokecard";
+import { useState } from "react";
 import "./Pokedex.css";
 
 export default function Pokedex(props) {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const handleSelectedIndex = (index) => {
+    setSelectedIndex((prevState) => (prevState === index ? null : index));
+  };
+
   return (
     <div className="pokedex-container">
       {props.data.map((e, i) => (
-        <Pokecard key={i} {...e} />
+        <Pokecard
+          isHidden={selectedIndex === i}
+          buttonClick={() => handleSelectedIndex(i)}
+          key={i}
+          {...e}
+        />
       ))}
     </div>
   );
